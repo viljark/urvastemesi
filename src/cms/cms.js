62810b6760b93624a7cf1d94490cb1fe46785cfb
Viljar Kärgenberg <viljark@gmail.com>
2018-12-18 02:53:28 +0200

    fix build errors

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,6 +3,7 @@ import CMS from 'netlify-cms'
 import GalleryPagePreview from './preview-templates/GalleryPagePreview'
 import ProductsPagePreview from './preview-templates/ProductsPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
+import BlogPostPreview  from './preview-templates/BlogPostPreview'
 
 CMS.registerPreviewTemplate('galerii', GalleryPagePreview)
 CMS.registerPreviewTemplate('ajalugu', ProductsPagePreview)
